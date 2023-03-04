f1372de8f0c79cea5ef094370b6eb0ff3f6b2f86
dibkb <dibas9110@gmail.com>
2023-03-04 13:56:52 +0530

    input fields

---
+++ components/Input.tsx
@@ -0,0 +1,61 @@
+import React, { ReactHTMLElement } from "react";
+interface InputProps {
+  id: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type: string;
+}
+const Input: React.FC<InputProps> = ({
+  id,
+  placeholder,
+  value,
+  onChange,
+  type,
+}) => {
+  return (
+    <div className="relative">
+      <input
+        id={id}
+        type={type}
+        className="bg-zinc-700 w-full rounded-md text-base outline-none
+              px-6
+              pt-6
+              pb-1
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
+        invalid:border-b-1
+         "
+        placeholder=" "
+        value={value}
+        onChange={onChange}
+      />
+      <label
+        htmlFor={id}
+        className="
+        absolute 
+        text-sm
+      text-zinc-400
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-9 
+        origin-[0] 
+        left-6
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      "
+      >
+        {placeholder}
+      </label>
+    </div>
+  );
+};
+
+export default Input;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import Input from "@/components/Input";
+import React, { useState } from "react";
 import logo from "../public/images/logo.png";
 const Auth: React.FunctionComponent = () => {
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
     <div className="relative w-full h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover bg-right">
       <div className=" bg-black w-full h-full lg:bg-opacity-50">
@@ -10,14 +14,26 @@ const Auth: React.FunctionComponent = () => {
         <section className="flex justify-center">
           <div
             className=" bg-black bg-opacity-80 rounded-md
-          lg:w-2/5 lg:max-w-md px-12 py-8 select-none flex flex-col gap-6"
+          lg:w-2/5 lg:max-w-md px-12 py-16 select-none flex flex-col gap-6"
           >
-            <h3 className="font-bold text-4xl">Sign In</h3>
-            <input
+            <h3 className="font-bold text-4xl mb-4">Sign In</h3>
+            <Input
+              id="email"
               type="text"
-              className="bg-zinc-700 rounded-md px-4 py-2 text-sm"
+              value={email}
               placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="bg-red-700 rounded-md py-3 hover:bg-red-800 mt-6">
+              Login
+            </button>
           </div>
         </section>
       </div>
