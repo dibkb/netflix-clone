4cc1b11553594d53460ffd1511e99f0da32c0fe2
dibkb <dibas9110@gmail.com>
2023-03-04 19:04:49 +0530

    original netflix background🍻

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -11,10 +11,9 @@ const Auth: React.FunctionComponent = () => {
   const switchVariant = useCallback(() => {
     setVariant((prev) => (prev === "register" ? "login" : "register"));
   }, []);
-
   return (
-    <div className="relative w-full h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover bg-right">
-      <div className=" bg-black w-full h-full lg:bg-opacity-50">
+    <div className="relative w-full h-full bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/a43711df-c428-4f88-8bb3-b2ac5f20608f/32935458-d049-44c2-b94b-32f16d60ded1/IN-en-20230227-popsignuptwoweeks-perspective_alpha_website_large.jpg')] bg-no-repeat bg-cover bg-center">
+      <div className=" bg-black w-full h-full lg:bg-opacity-60">
         <nav className="px-12 py-8">
           <img src={logo.src} alt="Logo" className="h-12" />
         </nav>
---
+++ utils/utils.ts
@@ -0,0 +1,4 @@
+export const netflixBgImage: any =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/a43711df-c428-4f88-8bb3-b2ac5f20608f/32935458-d049-44c2-b94b-32f16d60ded1/IN-en-20230227-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+export const netflixBgImage_2: string =
+  "https://raw.githubusercontent.com/AntonioErdeljac/next-netflix-tutorial/master/public/images/hero.jpg";
