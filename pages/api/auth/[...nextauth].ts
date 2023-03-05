ea64e759520b3478258dbe83dd2d7931277d760e
dibkb <dibas9110@gmail.com>
2023-03-05 12:31:51 +0530

    new auth settings

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,31 +3,35 @@ import { compare } from "bcrypt";
 import prismadb from "../../../libs/prismadb";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import Credentials from "next-auth/providers/credentials";
-export const authOptions = {
-  adapter: PrismaAdapter(prismadb),
+export default NextAuth({
   providers: [
     Credentials({
-      id: "Credentials",
+      id: "credentials",
       name: "Credentials",
       credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Password",
+          type: "passord",
+        },
       },
-      async authorize(credentials, req) {
-        const { email, password } = credentials;
-        if (!email || !password) {
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
           throw new Error("Email and Password required");
         }
         const user = await prismadb.user.findUnique({
           where: {
-            email,
+            email: credentials.email,
           },
         });
         if (user) {
           // Any object returned will be saved in `user` property of the JWT
           const isCorrectPassword = await compare(
-            password,
-            user.hashedPassword
+            credentials.password,
+            user.hashedPassword as string
           );
           if (!isCorrectPassword) {
             throw new Error("Password does not match");
@@ -35,7 +39,7 @@ export const authOptions = {
           return user;
         } else {
           // If you return null then an error will be displayed advising the user to check their details.
-          throw new Error("Email does not exist");
+          throw new Error("User does not exist");
           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
@@ -45,11 +49,10 @@ export const authOptions = {
     signIn: "/auth",
   },
   debug: process.env.NODE_ENV === "development",
+  adapter: PrismaAdapter(prismadb),
   session: { strategy: "jwt" },
   jwt: {
     secret: process.env.NEXTAUTH_JWT_SECRET,
   },
   secret: process.env.NEXTAUTH_SECRET,
-};
-
-export default NextAuth(authOptions);
+});
