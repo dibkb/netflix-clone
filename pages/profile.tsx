d03f16747e0c74e4d4941f32e9e97b5b70ebff24
dibkb <dibas9110@gmail.com>
2023-03-06 17:08:44 +0530

    profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,11 +1,44 @@
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import { NextPageContext } from "next";
+import { getSession } from "next-auth/react";
 import React from "react";
 import logo from "../public/images/logo.png";
-const profile = () => {
+import { users } from "@/utlis/usernames";
+export async function getServerSideProps(conetxt: NextPageContext) {
+  const session = await getSession(conetxt);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
+const App: React.FC = () => {
+  const { data: user } = useCurrentUser();
+  console.log(user);
   return (
-    <div className="select-none flex flex-col">
-      <img src={logo.src} alt="Logo" className="lg:h-24 h-12" />
+    <div className="select-none flex flex-col mx-auto w-full py-32">
+      <img src={logo.src} alt="Logo" className="md:h-16 h-10 w-min mx-auto" />
+      <h1 className="text-4xl md:text-6xl text-center mt-12">
+        Who&apos;s watching?
+      </h1>
+      <div className="flex gap-12 mt-12 mx-auto w-fit flex-wrap">
+        {users.map((account, i) => {
+          return (
+            <span key={i}>
+              <img src={account.thubmnail} alt="" className="h-32 mb-2" />
+              <p className="text-center">{account.name || user?.name}</p>
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default profile;
+export default App;
---
+++ utlis/usernames.ts
@@ -0,0 +1,21 @@
+export const users = [
+  {
+    thubmnail:
+      "https://raw.githubusercontent.com/AntonioErdeljac/next-netflix-tutorial/master/public/images/default-blue.png",
+    name: "Bruce Wayne",
+  },
+  {
+    thubmnail:
+      "https://raw.githubusercontent.com/AntonioErdeljac/next-netflix-tutorial/master/public/images/default-green.png",
+  },
+  {
+    thubmnail:
+      "https://raw.githubusercontent.com/AntonioErdeljac/next-netflix-tutorial/master/public/images/default-red.png",
+    name: "Clark Kent",
+  },
+  {
+    thubmnail:
+      "https://raw.githubusercontent.com/AntonioErdeljac/next-netflix-tutorial/master/public/images/default-slate.png",
+    name: "Princess Dianna",
+  },
+];
