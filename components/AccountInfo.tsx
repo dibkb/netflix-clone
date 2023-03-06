50eb0bb32ae99b75eb524fc620fe853507eb47b1
dibkb <dibas9110@gmail.com>
2023-03-06 19:05:55 +0530

    account show hide

---
+++ components/AccountInfo.tsx
@@ -0,0 +1,28 @@
+import { signOut } from "next-auth/react";
+import React from "react";
+interface AccountInfo {
+  showMenu: boolean;
+}
+const AccountInfo: React.FC<AccountInfo> = ({ showMenu }) => {
+  const profileImage = localStorage.getItem("netflix-clone-picture");
+  const profileName = localStorage.getItem("netflix-clone-username");
+  if (!showMenu) return null;
+  return (
+    <div className="absolute rounded-md border-2 border-gray-800 top-12 bg-zinc-900 right-0 w-56">
+      <span className="flex gap-3 items-center border-b-2 border-gray-800 p-4">
+        <img src={profileImage} alt="Profile pic" className="h-8 rounded-md" />
+        <p className="text-sm text-zinc-300 cursor-pointer hover:underline">
+          {profileName}
+        </p>
+      </span>
+      <button
+        onClick={() => signOut()}
+        className="p-4 text-sm text-zinc-300 cursor-pointer hover:underline w-full"
+      >
+        Sign Out of Netfix
+      </button>
+    </div>
+  );
+};
+
+export default AccountInfo;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import logo from "../public/images/logo.png";
 import { SlMagnifier } from "react-icons/sl";
 import { HiOutlineChevronDown } from "react-icons/hi";
 import { MdOutlineNotificationsNone } from "react-icons/md";
+import AccountInfo from "./AccountInfo";
 interface NavItem {
   label: string;
 }
@@ -15,7 +16,7 @@ const NavItem: React.FC<NavItem> = ({ label }) => {
 };
 const Navbar: React.FC = () => {
   const profileImage = localStorage.getItem("netflix-clone-picture");
-  const profileName = localStorage.getItem("netflix-clone-username");
+  const [showAccountMenu, setShowAccountMenu] = React.useState<boolean>(false);
   return (
     <nav className="w-full fixed select-none">
       <div className="px-4 md:px-16 py-8 gap-8 flex items-center justify-items-stretch">
@@ -28,12 +29,21 @@ const Navbar: React.FC = () => {
           <NavItem label={"My List"} />
           <NavItem label={"Browse by Language"} />
         </div>
-        <aside className="ml-auto flex gap-8 items-center">
+        <aside className="ml-auto flex gap-8 items-center relative">
+          <AccountInfo showMenu={showAccountMenu} />
           <SlMagnifier size={18} className="cursor-pointer" />
           <MdOutlineNotificationsNone size={24} className="cursor-pointer" />
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 relative">
             <img src={profileImage} alt="" className="h-8 rounded-md" />
-            <HiOutlineChevronDown size={18} className="cursor-pointer" />
+            <HiOutlineChevronDown
+              onClick={() => {
+                setShowAccountMenu((prev) => !prev);
+              }}
+              size={18}
+              className={`cursor-pointer transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
           </div>
         </aside>
       </div>
