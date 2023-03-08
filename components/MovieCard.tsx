06f9ba4522db4b9ab00cb484e379d737f28ffd5d
dibkb <dibas9110@gmail.com>
2023-03-08 10:12:51 +0530

    movie card

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { GoPlay } from "react-icons/go";
-import { MdOutlineFavoriteBorder } from "react-icons/md";
+import { BookmarkIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { PlayCircleIcon } from "@heroicons/react/24/solid";
 interface MovieCard {
   id: string;
   thumbnailUrl: string;
   title: string;
+  duration: string;
 }
-const MovieCard: React.FC<MovieCard> = ({ id, thumbnailUrl, title }) => {
+const MovieCard: React.FC<MovieCard> = ({
+  id,
+  thumbnailUrl,
+  title,
+  duration,
+}) => {
   return (
     <main key={id} className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -55,14 +62,20 @@ const MovieCard: React.FC<MovieCard> = ({ id, thumbnailUrl, title }) => {
             w-full
             h-[12vw]"
         />
-        <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full shadow-md rounded-b-md">
-          <span className="flex gap-3">
-            <GoPlay size={45} className="cursor-pointer hover:text-gray-200" />
-            <MdOutlineFavoriteBorder
-              size={45}
-              className="cursor-pointer font-thin hover:text-gray-200"
-            />
+        <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full shadow-md rounded-b-md flex flex-col gap-y-2">
+          <div className="flex items-center w-full">
+            <span className="flex gap-3 items-center">
+              <PlayCircleIcon className="h-6 w-6 md:h-10 md:w-10 cursor-pointer hover:text-gray-200" />
+              <BookmarkIcon className="h-6 w-6 md:h-8 md:w-8 cursor-pointer hover:text-gray-200" />
+            </span>
+            <ChevronDownIcon className="h-6 w-6 md:h-8 md:w-8 ml-auto cursor-pointer hover:text-gray-200" />
+          </div>
+          <p className="text-xl">{title}</p>
+          <span className="flex gap-2">
+            <p className="text-green-400">New</p>
+            2023
           </span>
+          <p>{duration}</p>
         </div>
       </div>
     </main>
